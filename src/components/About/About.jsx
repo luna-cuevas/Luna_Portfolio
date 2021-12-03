7862380f947092f96bb8df50e15a69ac03234c0c
Luna Cuevas <s.cuevas14@gmail.com>
2021-12-03 13:46:53 -0800

    finalizing updates!
    just need to add recent project images :)
    
    during this session I updated meta tags and designed a new lil icon for myself ^.^

--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -1,7 +1,6 @@
 import React, { useState } from 'react'
 import './AboutStyle.css'
 import me from '../img/me.png'
-import Button from '../Button/Button'
 import react  from '../img/react.svg'
 import javascript from '../img/javascript.svg'
 import html5 from '../img/html5.svg'
@@ -9,18 +8,13 @@ import css3 from '../img/css.svg'
 import sass from '../img/sass.svg'
 import bootstrap from '../img/bootstrap.svg'
 import node from '../img/nodejs.svg'
-import que from '../img/que.svg'
 import github from '../img/github.svg'
 import photoshop from '../img/photoshop.svg'
 import illustrator from '../img/illustrator.svg'
 import mongo from '../img/mongoDB.svg'
 import material from '../img/material.svg'
-import resume from '../misc./Luna.Cuevas.prod.pdf'
-import { Link } from 'react-router-dom'
 
 const About = () => {
-
-
     return (
         <div className='info' id='info'>
             <h1 className="title">About Me</h1>
@@ -29,15 +23,13 @@ const About = () => {
                     <img id="pic" src={me} alt="a picture of me, Luna Cuevas"></img>
                     <div className='lorem'>
                         <p>Hello! Thanks for taking a moment to check out my website!</p>
-                        <p>I'm an aspiring full-stack developer working on their second year of self-teaching web development. I use Udemy courses, bootcamp material, Youtube tutorials, and <i>plenty</i> of books to reinforce the fundamentals of web development. I've also gained a community of developers through online forums and discord servers. </p>
-                        <p>In the past year, I've found success as a freelance web developer working with e-commerce clients and content creators. I draft, design, and build beautiful mobile-responsive web pages with SEO optimization and cross browser compatibility. I can also modify CSS and PHP code in CMS tools like Wordpress, Wix, and Squarespace. </p>
-                        {/* <p>The reason I embarked on this journey was because I found myself at a crossroads in my life. After graduating from U.C. Berkeley with a degree in Political Science, and working with startups in a variety of roles for 4 years, I realized that I needed the sense of fulfillment that comes with a career. I wanted my work to have a meaningful impact. And as someone obsessed with technology and its socio-political impact, I decided to learn to code. </p> */}
-                        {/* <p>This portfolio exists as an example of my design and development skills. The goal is to find a team of talented engineers to take a chance and allow me to demonstrate my capabilities to design and develop great features, applications, and user experiences. Regardless of your position, I would really appreciate any feedback, comments, or mentorship you wish to give.</p> */}
-                        <p>You can check out my projects and work below. Thanks again for taking time to read through this, you're awesome!</p>
+                        <p>I'm a front end developer with over two years of self-taught knowledge and a few months of professional experience. I use Udemy courses, bootcamp material, Youtube tutorials, and <i>plenty</i> of books to reinforce the fundamentals of web development. I've also gained a community of developers through online forums and discord. </p>
+                        <p>In the past year, I've found success as a freelance web developer working with e-commerce clients and content creators. I draft, design, and build beautiful mobile-responsive web pages with SEO optimization and cross browser compatibility. I can also modify CSS and PHP code in CMS tools like Wordpress, Wix, and Squarespace.</p>
+                        <p>My goal is to find a group of talented engineers and help tackle modern problems with creative solutions. You can check out my projects and work below. Thanks again for taking time to read through this, you're awesome!</p>
                     </div>
                 </div>
                 <div className='skills'>
-                    <h2>Skills</h2>
+                    <h2 className='skills-title'>Skills</h2>
                     <div className='icons'>
                         <p>React <img src={react} alt="react icon" /></p>
                         <p>JavaScript <img src={javascript} alt="" /></p>
@@ -50,7 +42,13 @@ const About = () => {
                         <p>GitHub <img src={github} alt="" /></p>
                         <p>Photoshop <img src={photoshop} alt="" /></p>
                         <p>Illustrator <img src={illustrator} alt="" /></p>
-                        <p>Material-UI <img src={material} alt="" /></p>
+                        <p>Material<img src={material} alt="" /></p>
+                    </div>
+                    <h2 className='skills-title'>In Progress</h2>
+                    <div className='icons'>
+                        <p>React <img src={react} alt="react icon" /></p>
+                        <p>JavaScript <img src={javascript} alt="" /></p>
+                        <p>HTML5 <img src={html5} alt="" /></p> 
                     </div>
                 </div>
             </div>
