dcc0ca8598f561ff55658873575020b71e90b320
Luna Cuevas <68247976+luna-cuevas@users.noreply.github.com>
2023-06-01 16:51:26 -0700

    updates to gtag

--- pages/_app.jsx
+++ pages/_app.jsx
@@ -1,12 +1,29 @@
-import '../styles/globals.css'
-import Layout from '../components/Layout'
+import "../styles/globals.css";
+import Layout from "../components/Layout";
+import { useRouter } from "next/router";
+import * as gtag from "../lib/gtag";
+import { useEffect } from "react";
 
 function MyApp({ Component, pageProps }) {
+  const router = useRouter();
+
+  useEffect(() => {
+    const handleRouteChange = (url) => {
+      gtag.pageview(url);
+    };
+
+    router.events.on("routeChangeComplete", handleRouteChange);
+
+    return () => {
+      router.events.off("routeChangeComplete", handleRouteChange);
+    };
+  }, [router.events]);
+
   return (
     <Layout>
       <Component {...pageProps} />
     </Layout>
-  )
+  );
 }
 
-export default MyApp
+export default MyApp;
