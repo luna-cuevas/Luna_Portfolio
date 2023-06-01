35dade13b36c71cda4ec6e47027446d5b0b1efc6
Luna Cuevas <68247976+luna-cuevas@users.noreply.github.com>
2023-06-01 16:47:47 -0700

    update to gtag

--- pages/_document.js
+++ pages/_document.js
@@ -20,20 +20,21 @@ export default function Document() {
         <script
           async
           src="https://www.googletagmanager.com/gtag/js?id=G-ED7F1Y8X3R"></script>
-        <script>
-          {`(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
-          new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
-          j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
-          'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-          })(window,document,'script','dataLayer','GTM-NRN22JF');`}
-        </script>
-        <script>
-          {`window.dataLayer = window.dataLayer || [];
+
+        <Script
+          id="google-analytics"
+          strategy="afterInteractive"
+          dangerouslySetInnerHTML={{
+            __html: `
+          window.dataLayer = window.dataLayer || [];
           function gtag(){dataLayer.push(arguments);}
           gtag('js', new Date());
-
-          gtag('config', 'G-ED7F1Y8X3R');`}
-        </script>
+          gtag('config', 'G-ED7F1Y8X3R', {
+            page_path: window.location.pathname,
+          });
+        `,
+          }}
+        />
 
         <title>Luna's Portfolio</title>
       </Head>
