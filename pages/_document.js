38a53e62b1150406ce2d84ee6a88fcf4706e09a7
Luna Cuevas <68247976+luna-cuevas@users.noreply.github.com>
2023-06-01 14:18:58 -0700

    Added gtag

--- pages/_document.js
+++ pages/_document.js
@@ -1,4 +1,5 @@
 import { Html, Head, Main, NextScript } from "next/document";
+import { useEffect } from "react";
 
 export default function Document() {
   return (
@@ -15,6 +16,18 @@ export default function Document() {
           content="This is a collection of my previous work and projects. Links to my Github and LinkedIn are available below."
         />
         <meta property="og:title" content="Luna's Portfolio" />
+
+        <script
+          async
+          src="https://www.googletagmanager.com/gtag/js?id=G-ED7F1Y8X3R"></script>
+        <script>
+          {`window.dataLayer = window.dataLayer || [];
+          function gtag(){dataLayer.push(arguments);}
+          gtag('js', new Date());
+
+          gtag('config', 'G-ED7F1Y8X3R');`}
+        </script>
+
         <title>Luna's Portfolio</title>
       </Head>
       <body>
