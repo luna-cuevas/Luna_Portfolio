0917c7caa7ac0cacafa6b974cd8a3dd1b086b55b
Luna Cuevas <68247976+luna-cuevas@users.noreply.github.com>
2023-06-12 11:52:04 -0700

    Updated copyright year

--- components/Footer.jsx
+++ components/Footer.jsx
@@ -1,11 +1,14 @@
-import React from 'react'
+import React from "react";
 
 const Footer = () => {
+  const currentYear = new Date().getFullYear();
   return (
-    <div className='border-t-2 flex py-2 w-screen h-fit z-50 bg-[#222] mt-8 border-gray-700'>
-      <div className='w-full m-auto text-center'>Copyright © 2022 <br /> Luna Cuevas</div>
+    <div className="border-t-2 flex py-2 w-screen h-fit z-50 bg-[#222] mt-8 border-gray-700">
+      <div className="w-full m-auto text-center">
+        Copyright © {currentYear} <br /> Luna Cuevas
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default Footer
+export default Footer;
